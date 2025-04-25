c02161a76ec3d002d223c747f4e40b8937ade3c5
Justin Fyfe <justin@fyfesoftware.ca>
2025-04-25 15:08:58 -0400

    Updated license headers

--- SanteDB.Messaging.HL7/Exceptions/HL7DatatypeProcessingException.cs
+++ SanteDB.Messaging.HL7/Exceptions/HL7DatatypeProcessingException.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (C) 2021 - 2024, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
+ * Copyright (C) 2021 - 2025, SanteSuite Inc. and the SanteSuite Contributors (See NOTICE.md for full copyright notices)
  * Copyright (C) 2019 - 2021, Fyfe Software Inc. and the SanteSuite Contributors
  * Portions Copyright (C) 2015-2018 Mohawk College of Applied Arts and Technology
  * 
@@ -15,6 +15,8 @@
  * License for the specific language governing permissions and limitations under 
  * the License.
  * 
+ * User: fyfej
+ * Date: 2023-6-21
  */
 using System;
 
